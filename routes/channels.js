b41e665b15dbad7cc904d89a28dec720013bc0b1
Amanda Heideman <heideman.amanda@gmail.com>
2021-03-24 21:43:17 +0100

    get channels from database

--- routes/channels.js
+++ routes/channels.js
@@ -5,22 +5,28 @@ const io = require('socket.io')(http);
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth')
 
-let channels = ['Frontend 1', 'Frontend 2', 'Random']
+
 const ChannelModel = require('../models/channels')
 
 //Renderar befintliga kaneler på startsidan
-router.get('/', ensureAuthenticated, (req, res) => { //anropar funktionen varje gång vi försöker gå till channels, kollar om vi är inlogade
+router.get('/', ensureAuthenticated, async(req, res) => { //anropar funktionen varje gång vi försöker gå till channels, kollar om vi är inlogade
   const user = req.user
   const name = user.name
 
+  const channels_from_db = await ChannelModel.find(); //hämtar alla channels från databas
   
-  res.render('channelList', { channels: channels, name: name }); //vi sparar name i en variabel och skickar med
+  res.render('channelList', { channels: channels_from_db, name: name }); //vi sparar name i en variabel och skickar med
+})
+
+router.get('/api', async(req, res) => { //skickar ett json till /channels/api med info från databasen
+  const channels_from_db = await ChannelModel.find();
+  res.json(channels_from_db)
 })
 
 //Lägger till nya kanaler som skapas
-router.post('/', ensureAuthenticated, (req, res) => {
+router.post('/', ensureAuthenticated, async(req, res) => {
   let channel = req.body.newChannel;
-  channels.push(channel);
+  //channels.push(channel);
 
   let channel_name = channel;
   const newChannel = new ChannelModel({ channel_name })
@@ -28,7 +34,9 @@ router.post('/', ensureAuthenticated, (req, res) => {
 
   const user = req.user
   const name = user.name
-  res.render('channelList', { channels: channels, name: name });
+
+  const channels_from_db = await ChannelModel.find();
+  res.render('channelList', { channels: channels_from_db, name: name });
 })
 
 
